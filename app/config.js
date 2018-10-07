34f41b0b625f46392dd50ffba681b7f4d20d91fd
Vivek S <keviveks@gmail.com>
2018-10-07 09:43:17 +0530

    Ask commit with a bot

---
+++ app/config.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+
+// check for .env file
+if (!fs.existsSync('.env')) {
+  console.log('Environment file not found!!');
+  process.exit(1);
+}
+
+/**
+ * Load the config parameters from .env file
+ */
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT,
+  auth: {
+    admin: process.env.AUTH_PASSWORD,
+  },
+  aiProjectId: process.env.AI_PROJECT_ID,
+  db: {
+    uri: `${process.env.MONGO_URI}/${process.env.MONGO_NAME}`,
+    options: {
+      keepAlive: 1,
+      useNewUrlParser: true,
+    },
+  },
+};
